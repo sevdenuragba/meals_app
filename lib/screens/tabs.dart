7881f93d4f7bc18695bad77ba7a93a452374c3df
Sevde Nur <sevdenuragba@gmail.com>
2023-08-23 10:20:49 +0300

    Initial commit

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -64,7 +64,7 @@ class _TabsScreenState extends State<TabsScreen> {
     if (identifier == 'filters') {
       final result = await Navigator.of(context).push<Map<Filter, bool>>(
         MaterialPageRoute(
-          builder: (ctx) => const FiltersScreen(),
+          builder: (ctx) =>  FiltersScreen(currentFilters: _selectedFilters,),
         ),
       );
       setState(() {
